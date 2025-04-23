5e162b329a6999a815c433da5d0e170d3a203474
Tomáš Selecký <tomas.selecky@cleverlance.com>
2025-04-23 21:26:13 +0200

    app title

--- lib/generic/strings.dart
+++ lib/generic/strings.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 class Strings {
-  static String app_title = 'Flutter Unity Helmet';
+  static String app_title = 'AR Gallery';
   // screen titles
   static String screen_title_home = 'screen_title_home';
   static String screen_title_helmet = 'screen_title_helmet';
